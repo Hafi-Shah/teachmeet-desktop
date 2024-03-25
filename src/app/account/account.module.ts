7c5aab1a07cfcedb1796984afac8fe5738bf35bc
hafi <devhafi1337@gmail.com>
2024-03-26 00:37:35 +0500

    ng

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { AccountComponent } from './account.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    RegisterComponent,
     AccountComponent
   ],
   imports: [
